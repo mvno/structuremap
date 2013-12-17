1cd8ebf48a834b6e0e360ccd0802e6781bc3d2e0
Kim Christensen <kich@mvno.dk>
2013-12-17 17:48:46 +0100

    Cleanup solution and add test project

---
+++ src/ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using StructureMap;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ObjectFactory.Initialize(x => x.Scan(scanner =>
+			{
+				scanner.AssemblyContainingType<ITest>();
+				scanner.WithDefaultConventions();
+			}));
+
+			var container = ObjectFactory.Container;
+
+			Parallel.For(0, 1000000, x =>
+			{
+				using (var nestedContainer = container.GetNestedContainer())
+				{
+					var instance = nestedContainer.GetInstance<ITest>();
+					instance.Index = x;
+				}
+			});
+		}
+	}
+
+	public interface ITest
+	{
+		int Index
+		{
+			get;
+			set;
+		}
+	}
+
+	public class Test : ITest
+	{
+		public int Index
+		{
+			get;
+			set;
+		}
+	}
+}
